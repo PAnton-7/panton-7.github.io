fb1b7bc001c8681d13fbd2e39a73d14dcfd414dd
PAnton-7 <121297166+PAnton-7@users.noreply.github.com>
2024-07-23 23:06:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-let tg = window.Telegram.WebApp
-tg.expand()
-tg.showScanQrPopup(text='QR')
+let tg = window.Telegram.WebApp;
+tg.expand();
+tg.showScanQrPopup(text="QR");
+
+Telegram.WebApp.onEvent("qrTextReceived", function(){
+  tg.sendData("888");
+});
